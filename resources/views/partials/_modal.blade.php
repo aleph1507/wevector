3fc34a2820cd8e21231a90dd92e04149d85ff130
xrristo <xrristo@gmail.com>
2018-12-04 15:55:58 +0100

    orders0001

--- resources/views/partials/_modal.blade.php
+++ resources/views/partials/_modal.blade.php
@@ -1,13 +1,13 @@
 <div class="modal" id="modal">
   <div class="modal-content">
     <span class="close">&times;</span>
-    <div></div>
+    <div style="width:100%;"></div>
   </div>
   <script type="text/javascript">
     function loadContent(content) {
       console.log('loadContent');
-      console.log(`<img src="${content}">`);
-      $(".modal-content > div").html(`<img src="${content}">`);
+      console.log(`<img src="${content}" >`);
+      $(".modal-content > div").html(`<img src=${content} class='img-fluid'>`);
     }
   </script>
 </div>
